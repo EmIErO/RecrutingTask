8b33e746e1511c3dfdb7467b33dbfe0fea188778
EmIErO <mira.bryja@gmail.com>
2019-07-10 12:38:34 +0200

    Refactor solution, implement stack-based solution and add tests.

--- src/main/java/BracketChecker.java
+++ src/main/java/BracketChecker.java
@@ -2,43 +2,50 @@ import java.util.*;
 
 public class BracketChecker {
 
-    public static Map<String, String> bracketPairs;
-    private List<String> openingBrackets = new ArrayList<>(Arrays.asList("(", "{", "["));
+    public static Map<Character, Character> bracketPairs;
+    private List<Character> openingBrackets = new ArrayList<>(Arrays.asList('(', '{', '['));
+    private List<Character> closingBrackets = new ArrayList<>(Arrays.asList(')', '}', ']'));
     private String brackets;
 
     static {
         bracketPairs = new HashMap<>();
-        bracketPairs.put("[", "]");
-        bracketPairs.put("(", ")");
-        bracketPairs.put("{", "}");
+        bracketPairs.put(']', '[');
+        bracketPairs.put(')', '(');
+        bracketPairs.put('}', '{');
     }
 
     public BracketChecker(String brackets) {
+        brackets = brackets.replaceAll("[^()\\[\\]{}]", "");
         this.brackets = brackets;
     }
 
     public boolean areBracketsMatchedAndNestedCorrectly() {
 
         if (brackets.length() % 2 == 1) {
-            return  false;
-        }
-        for (int i = 0; i < brackets.length() / 2; i++) {
-            String currentBrace = String.valueOf(brackets.charAt(i));
-            if (!openingBrackets.contains(currentBrace)) {
-                return false;
-            }
-            String closingBrace = bracketPairs.get(currentBrace);
-            String actualBrace = String.valueOf(brackets.charAt(brackets.length() - i - 1));
-            if(!actualBrace.equals(closingBrace)) {
-                return false;
-            }
+            return false;
         }
 
-        return true;
-    }
+        Stack<Character> stack = new Stack<>();
 
-    public static void main(String[] args) {
-        BracketChecker bracketChecker = new BracketChecker("({})");
-        System.out.println(bracketChecker.areBracketsMatchedAndNestedCorrectly());
+        for (int i = 0; i < brackets.length(); i++) {
+            char currentBracket = brackets.charAt(i);
+
+            if (openingBrackets.contains(currentBracket)) {
+                stack.push(currentBracket);
+            } else if (closingBrackets.contains(currentBracket)) {
+                if (stack.isEmpty()) { return false; }
+
+                char actualBracket = stack.peek();
+                char expectedBracket = bracketPairs.get(currentBracket);
+
+                if (actualBracket == expectedBracket) {
+                    stack.pop();
+                } else { return false; }
+            }
+        }
+        if (stack.isEmpty()) {
+            return true;
+        }
+        return false;
     }
 }
